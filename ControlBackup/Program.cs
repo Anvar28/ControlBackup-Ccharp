bae474c922e65c79d12e1ebce127de3effeedde1
Анвар <apxi2@yandex.ru>
2018-08-16 14:58:42 +0500

    1

--- ControlBackup/Program.cs
+++ ControlBackup/Program.cs
@@ -2,14 +2,95 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace ControlBackup
+enum tParamData { tboll, tstring }
+class cParamData
 {
-    class Program
+
+    public string command;
+    public tParamData type;
+    public string data;
+    public string descript;
+
+    public cParamData(string lCommand, tParamData ltype = tParamData.tstring, string defData = "", string lDescript = "")
     {
-        static void Main(string[] args)
+        command = lCommand;
+        type = ltype;
+        data = defData;
+        descript = lDescript;
+    }
+}
+
+class cParam : IEnumerable
+{
+    private Dictionary<string, cParamData> paramList;
+
+    void Param()
+    {
+        paramList = new Dictionary<string, cParamData>();
+    }
+
+    public void Add(string lCommand, tParamData ltype = tParamData.tstring, string defData = "", string lDescript = "")
+    {
+        paramList.Add(lCommand, new cParamData(lCommand, ltype, defData, lDescript));
+    }
+
+    public cParamData Get(string lCommand)
+    {
+        return paramList[lCommand];
+    }
+
+    public void Parse(string[] args)
+    {
+        // Заполним параметры из командной строки
+        int count = args.Count();
+        for (int i = 0; i < paramList.Count(); i++)
         {
+            var itemParam = paramList.ElementAt(i);
+            string key = itemParam.Key;
+            cParamData value = itemParam.Value;
+            for (int j = 0; j < count; j++)
+            {
+                if (key == args[j])
+                {
+                    if (value.type == tParamData.tboll)
+                    {
+                        value.data = "True";
+                    }
+                    else
+                    {
+                        // Следущий должно идти значение параметра
+                        if (j + 1 < count)
+                        {
+                            value.data = args[j + 1];
+                        }
+                    }
+                }
+            }
         }
+    }
+}
+
+internal class Program
+{    
+
+    static void Main(string[] args)
+    {
+
+        cParam param = new cParam();
+        param.Add("-f", tParamData.tboll, "", "-fileproperty");
+        param.Add("-s", tParamData.tstring, "", "-smtpserver");
+        param.Add("-u", tParamData.tboll, "", "-username");
+        param.Add("-p", tParamData.tboll, "", "-password");
+        param.Add("-t", tParamData.tboll, "", "-mailto");
+        param.Add("-su", tParamData.tboll, "", "-subject");
+        param.Add("-pb", tParamData.tboll, "", "-pathbackup");
+        param.Parse(args);
+
+        foreach (var item in param)
+        {
+            Console.WriteLine(item.Key + "    " + item.Value.data);
+        }
+        Console.ReadLine();
     }
 }
